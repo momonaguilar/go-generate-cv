65760d8ef3261f1df60444bf8670dbd9e0d9b30b
Jose @ GCP <jose.for.dev@gmail.com>
2022-01-09 16:30:31 +0000

    Initial commit

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	// "log"
+	// "net/http"
+)
+
+func main() {
+	fmt.Printf("Starting server at port 8080\n")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
